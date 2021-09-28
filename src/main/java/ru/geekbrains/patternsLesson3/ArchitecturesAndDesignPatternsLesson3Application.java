6b830c6d66e78d318c7c6b4d3a563931f067ddcb
Evengarr <Ravasect@gmail.ru>
2021-09-28 21:14:50 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/patternsLesson3/ArchitecturesAndDesignPatternsLesson3Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.patternsLesson3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturesAndDesignPatternsLesson3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitecturesAndDesignPatternsLesson3Application.class, args);
+    }
+
+}
